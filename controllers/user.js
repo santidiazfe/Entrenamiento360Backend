0935085a1e682bed31045d2c46a13c544728dd2a
santidiazfe <santidiazfe@gmail.com>
2023-03-04 19:17:42 -0300

    first commit

---
+++ controllers/user.js
@@ -0,0 +1,37 @@
+const configDB = require('../knexfile');
+const knex = require('knex')(configDB.development);
+
+
+
+const getAllUsers = () => {
+    return knex
+    .column('cedula', 'nombre', 'password')
+    .select()
+    .from('usario')
+}
+const getUsersById = (id) => {
+    return knex('usuario')
+    .where('cedula', id)
+    .select('nombre','cedula')
+}
+const createUser = (body) => {
+    return knex('usuario')
+    .insert(body)
+}
+const updateUser = (id, body) => {
+    return knex('usuario')
+    .where('cedula', id)
+    .update(body)
+}
+const deleteUser = (id) => {
+    return knex('usuario')
+    .where('cedula', id)
+    .del()
+}
+module.exports = {
+    deleteUser,
+    updateUser,
+    createUser,
+    getAllUsers,
+    getUsersById
+}
