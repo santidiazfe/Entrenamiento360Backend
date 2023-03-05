10d800f4a4873d2efe128cdfea909668b39526dc
santidiazfe <santidiazfe@gmail.com>
2023-03-04 22:58:03 -0300

    changes in auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,69 +2,62 @@ const express = require('express');
 const configDB = require('../knexfile');
 const knex = require('knex')(configDB.development);
 const bcrypt = require('bcrypt');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
-const {verifyToken, TOKEN_SECRET} = require('../middlewares/jwt');
+const { verifyToken, TOKEN_SECRET } = require('../middlewares/jwt');
 const router = express.Router();
 
 
-// app.use(bodyParser.urlencoded({extended:true}));
 router.post('/login', async (req, res) => {
-    //buscamos usuario con mismo mail
-    console.log(req.body, 'req.body')
-    try {
-    const user = 
-    await knex
-    .select('*')
-    .from('usuario')
-    .where('user.nombre', req.body.name)
-    .then((user) => {
+  console.log(req.body, 'req.body');
+  try {
+    const user = await knex
+      .select('*')
+      .from('usuario')
+      .where('usuario.nombre', req.body.name)
+      .then((user) => {
+        console.log(user, 'user');
         return user;
-    })
-    console.log(user, 'user')
-    if(!user) {
-        return res.status(400).json({error: 'Usuario no encontrado', success:false});
+      })
+      .catch((error) => {
+        console.log("Error en la consulta a la base de datos", error);
+        res.status(500).json({ error: "Error en la consulta a la base de datos", success: false });
+        return;
+      });
+
+    if (!user || user.length === 0) {
+      return res.status(400).json({ error: 'Usuario no encontrado', success: false });
     }
-    const validPassword = bcrypt.compareSync(  req.body.password, user[0].password );
+    const validPassword = bcrypt.compareSync(req.body.password, user[0].password);
     console.log(validPassword)
-    if(!validPassword) {
-        return res.status(400).json({error: 'Contraseña no válida', success:false});
+    if (!validPassword) {
+      return res.status(400).json({ error: 'Contraseña no válida', success: false });
     }
-    //Crear el token
     const token = jwt.sign({
-        name: user[0].name,
-        id: user[0].id
+      name: user[0].name,
+      id: user[0].id
     }, TOKEN_SECRET);
-    res.json({error: null, data: 'Login exitoso', token, success:true});
-    } catch (error) {
-        console.log("Error en login");
-      }
+    res.json({ error: null, data: 'Login exitoso', token, success: true });
+  } catch (error) {
+    console.log("Error en login");
+    res.status(500).json({ error: "Error en el servidor", success: false });
+  }
 });
- //Registro de un usuario
-router.post('/register', (req, res) => {
-    const password = req.body.password;
+
+router.post('/register', async (req, res) => {
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const password = bcrypt.hashSync(req.body.password, salt);
     const newUser = {
-        name: req.body.name,
-        password //linea 52 se hace el hash
+      name: req.body.name,
+      password: password
     }
-    usuarios.push(newUser);
-    res.json({success: true, newUser, usuarios}) //Devuelve contraseña y usuario en postman
-})   
-router.post('/', async (req, res) => {
-    // hash contraseña
-    const salt =  bcrypt.genSaltSync(10);
-    const password =  bcrypt.hashSync(req.body.password, salt);
-    const newUser = {
-        name: req.body.name,
-        password: password
-    } 
-    console.log(newUser, 'newUser')
-    return await knex('user')
-    .insert(newUser)
-    .then((res) => {
-        res.status(200).json({success: true, newUser, res}); // success:true preguntar richard
-    }).catch((err) => {
-        res.status(400).json({err: err})
-    })
-})    
+    await knex('usuario').insert(newUser);
+    res.status(200).json({ success: true, newUser });
+  } catch (error) {
+    console.error(error);
+    res.status(400).send("Error al registrar usuario");
+  }
+});
+
 module.exports = router;
