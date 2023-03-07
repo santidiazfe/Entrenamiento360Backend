8e79b7b53883d08f515daa0436b54bd15607cc45
santidiazfe <santidiazfe@gmail.com>
2023-03-07 19:19:27 -0300

    catch in register

--- routes/register.js
+++ routes/register.js
@@ -7,28 +7,21 @@ const router = express.Router();
 
 
 router.post('/', async (req, res) => {
-    try {
-      const salt = bcrypt.genSaltSync(10);
-      const password = bcrypt.hashSync(req.body.password, salt);
-      const newUser = {
-        cedula: req.body.cedula,
-        password: password,
-        email: req.body.email,
-        rol_id: req.body.rol_id
-      };
-      await knex('usuario').insert(newUser);
-      res.status(200).json({ success: true, newUser });
-    } catch (error) {
-      if (error.code === '23505') {
-          res.status(400).json({ error: 'El usuario ya existe', success: false });
-          return;
-      } else {
-          res.status(500).json({ error: 'Error interno del servidor', success: false });
-          return;
-      }
-      console.error(error);
-      res.status(400).json({ error: true, message: error.message });
-    }
-  });
+  try {
+    const salt = bcrypt.genSaltSync(10);
+    const password = bcrypt.hashSync(req.body.password, salt);
+    const newUser = {
+      cedula: req.body.cedula,
+      password: password,
+      email: req.body.email,
+      rol_id: req.body.rol_id
+    };
+    await knex('usuario').insert(newUser);
+    res.status(200).json({ message: 'Usuario creado exitosamente.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Ocurrió un error en el servidor.' });
+  }
+});
 
 module.exports = router;
